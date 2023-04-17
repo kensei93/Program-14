b75e6e682766513d4853a70388cc53a61c9e4092
Vyacheslav Zaitsev <110720822+kensei93@users.noreply.github.com>
2023-04-18 01:54:24 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,7 @@
 package ru.netology.service;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
