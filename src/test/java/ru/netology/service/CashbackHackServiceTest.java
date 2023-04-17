eedf75f4110bbe5c1366941e4d3bd9d154a4d82e
Vyacheslav Zaitsev <110720822+kensei93@users.noreply.github.com>
2023-04-18 02:41:13 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,40 +7,36 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
-    public void testRemainBoundaryValuesOne() {
-        org.testng.Assert.assertThrows(RuntimeException.class, () -> {
-            service.remain(-1);
-        });
+    @Test
+    public void shouldCalculateIfAmountCorrect() {
+        int amount = 5700;
+        int actual = service.remain(amount);
+        int expected = 300;
+        assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
-    public void testRemainBoundaryValuesTwo() {
-        org.testng.Assert.assertEquals(service.remain(0), 1000);
+    @Test
+    public void shouldCalculateIfAmountZero() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
-    public void testRemainBoundaryValuesThree() {
-        org.testng.Assert.assertEquals(service.remain(1), 999);
+    @Test
+    public void shouldCalculateIfAmount1000() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
-    public void testRemainBoundaryValuesFour() {
-        org.testng.Assert.assertEquals(service.remain(999), 1);
+    @Test
+    public void shouldCalculateIfAmountIncorrect() {
+        int amount = -50000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
-    public void testRemainBoundaryValuesFive() {
-        org.testng.Assert.assertEquals(service.remain(1000), 0);
-    }
-
-    @org.testng.annotations.Test
-    public void testRemainBoundaryValuesSix() {
-        org.testng.Assert.assertEquals(service.remain(1001), 999);
-    }
-
-    @org.testng.annotations.Test
-    public void testRemainCriticalPath() {
-        org.testng.Assert.assertEquals(service.remain(400), 600);
-    }
 }
