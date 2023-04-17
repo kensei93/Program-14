e31de62f5f1360d6b8ac674e0454b4a8d2719d24
Vyacheslav Zaitsev <110720822+kensei93@users.noreply.github.com>
2023-04-18 02:43:20 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,44 +1,41 @@
 package ru.netology.service;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.junit.Test
-    public void testRemainBoundaryValuesOne() {
-        org.junit.Assert.assertThrows(RuntimeException.class, () -> {
-            service.remain(-1);
-        });
+    @Test
+    public void shouldCalculateIfAmountCorrect() {
+        int amount = 5700;
+        int actual = service.remain(amount);
+        int expected = 300;
+        assertEquals(expected, actual);
     }
 
-    @org.junit.Test
-    public void testRemainBoundaryValuesTwo() {
-        org.junit.Assert.assertEquals(1000, service.remain(0));
+    @Test
+    public void shouldCalculateIfAmountZero() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
     }
 
-    @org.junit.Test
-    public void testRemainBoundaryValuesThree() {
-        org.junit.Assert.assertEquals(999, service.remain(1));
+    @Test
+    public void shouldCalculateIfAmount1000() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
     }
 
-    @org.junit.Test
-    public void testRemainBoundaryValuesFour() {
-        org.junit.Assert.assertEquals(1, service.remain(999));
-    }
-
-    @org.junit.Test
-    public void testRemainBoundaryValuesFive() {
-        org.junit.Assert.assertEquals(0, service.remain(1000));
-    }
-
-    @org.junit.Test
-    public void testRemainBoundaryValuesSix() {
-        org.junit.Assert.assertEquals(999, service.remain(1001));
-    }
-
-    @org.junit.Test
-    public void testRemainCriticalPath() {
-        org.junit.Assert.assertEquals(600, service.remain(400));
+    @Test
+    public void shouldCalculateIfAmountIncorrect() {
+        int amount = -50000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
     }
 }
